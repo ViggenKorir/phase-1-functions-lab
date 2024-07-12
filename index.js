259b2c38306e0e2e05962ad17bdc807a110c1d85
Viggen <koriremanuel0@gmail.com>
2024-07-12 17:54:14 +0300

    Update

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let totalFeet = distanceTravelledInFeet(start, destination);
+    if (totalFeet < 400) {
+        return 0;
+    } else if (totalFeet >= 400 && totalFeet <= 2000) {
+        return (totalFeet - 400) * 0.02;
+    } else if (totalFeet > 2000 && totalFeet < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
